f7fae9420761fc2f5c8e66409c5cf2aae9136c89
Nwagu David-Isaac Okenwa <okenwa.nwagu@mcis.sch.ng>
2025-03-27 11:53:18 +0100

    lesson05

---
+++ lesson05.js
@@ -0,0 +1,7 @@
+alert("Hello " +"World");
+var message = "Hello";
+var name = prompt("Your name");
+alert(message + " there, " + name);alert("Hello " +"World");
+var message = "Hello";
+var name = prompt("Your name");
+alert(message + " there, " + name);
